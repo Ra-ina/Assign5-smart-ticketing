9564d7d959172e878efd6b6f7c2d05024c282990
Ra-ina <rainajahan12345@gmail.com>
2024-02-19 04:35:07 +0600

    tried to complete

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,76 @@
 // console.log('connected');
 
+let count = 1;
+let totalPrice = 0;
+const allBtns = document.getElementsByClassName('btn1');
 
+for (const btn1 of allBtns) {
+    // SET THE TICKET TO 550
+    btn1.addEventListener('click', function () {
+        btn1.classList.add('bg-green-300');
+        const settingTicket = document.getElementById('ticketSet');
+        settingTicket.innerText = + count;
+        count++;
 
+        // THE TICKET PRICE
+        const ticketPrice = document.getElementById('priceOfTicket');
+        const ticket = ticketPrice.innerText;
+        const convertTypeofTicket = parseInt(ticket);
+        document.getElementById('priceOfTicket').innerText = parseInt(ticket);
 
+        const btnText = btn1.innerText
 
-// THE TICKET PRICE
-const ticketPrice = document.getElementById('priceOfTicket');
-const ticket = ticketPrice.innerText;
-const convertTicket = parseInt(ticket);
-document.getElementById('priceOfTicket').innerText = parseInt(ticket);
+        // SET THE PRICE
+        const settingPrice = document.getElementById('priceSet');
+        const li = document.createElement('li')
+        li.style.display = 'flex'
+        li.style.justifyContent = "space-between"
+        const para = document.createElement('p');
+        para.innerText = btnText;
+        const para2 = document.createElement('p');
+        para2.innerText = convertTypeofTicket;
+        li.appendChild(para)
+        li.appendChild(para2)
+        settingPrice.appendChild(li);
 
-const seatText=seat.innerText
+        // SET TOTAL PRICE
+        totalPrice = totalPrice + convertTypeofTicket;
+        document.getElementById('totalPrice').innerText = totalPrice;
+
+        const grandTotal = document.getElementById('grandTotal')
+        const grandTotalPrice = grandTotal.innerText;
+        const convertTypeofTotal = parseInt(grandTotalPrice);
+
+        const seatsLeft = document.getElementById('seatsLeft');
+        const seatsLeftText = seatsLeft.innerText;
+        const convertTypeofSeatsLeft = parseInt(seatsLeftText);
+        const newSeats = convertTypeofSeatsLeft - 1;
+        seatsLeft.innerText = newSeats;
+
+    })
+
+}
+// COUPON PART BUT ITS NOT WORKING YET.
+const couponBtn = document.getElementById('btnApply');
+couponBtn.addEventListener('click', function () {
+    const couponCode = document.getElementById('coupon-input').value;
+    const input = couponCode.split(' ').join('').toUpperCase();
+    if (couponCode === 'NEW15') {
+        grandTotal = grandTotal - (ticketPrice * 0.15);
+        setInnerText('grandTotal', grandTotal);
+        document.getElementById('coupon-field').classList.add('hidden');
+    }
+    else if (couponCode === 'Couple 20') {
+
+        grandTotal = grandTotal - (ticketPrice * 0.20);
+        setInnerText('grandTotal', grandTotal);
+        document.getElementById('coupon-field').classList.add('hidden');
+    }
+    else {
+        alert('invalid coupon code');
+    }
+})
+
 
 // next and return button functions
 const nextBtn = document.getElementById('NextBtn');
