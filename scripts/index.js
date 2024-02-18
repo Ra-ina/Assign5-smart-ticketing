bcc0e4aece04d7bddfbddaffb3429c769ed64f1c
Ra-ina <rainajahan12345@gmail.com>
2024-02-19 02:07:31 +0600

    js functions started

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,27 @@
-console.log('connected');
+// console.log('connected');
+
+
+
+
+
+// THE TICKET PRICE
+const ticketPrice = document.getElementById('priceOfTicket');
+const ticket = ticketPrice.innerText;
+const convertTicket = parseInt(ticket);
+document.getElementById('priceOfTicket').innerText = parseInt(ticket);
+
+const seatText=seat.innerText
+
+// next and return button functions
+const nextBtn = document.getElementById('NextBtn');
+nextBtn.addEventListener('click', function () {
+    document.getElementById('header').classList.add('hidden');
+    document.getElementById('main').classList.add('hidden');
+    document.getElementById('footer').classList.add('hidden');
+    document.getElementById('successPop').classList.remove('hidden');
+
+});
+const returnBtn = document.getElementById('ReturnBtn');
+returnBtn.addEventListener('click', function () {
+    location.reload()
+});
